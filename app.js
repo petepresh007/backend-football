db6c1b324ac02f54f6fb7b6268aed70b34071a12
petepresh007 <preshtech18@gmail.com>
2024-05-08 10:43:46 -0800

    cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("express-async-errors")
 require("dotenv").config()
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
 const port = process.env.PORT || 5001;
 const connectDB = require('./db/connectDB');
 const errorHandler = require("./middleware/errorHandler");
@@ -18,9 +19,14 @@ mongoose.set('strictQuery', false);
 app.use(express.json());
 app.use(cooker_parser());
 app.use("/upload", express.static(path.join(__dirname, "upload")))
+// app.use(cors({
+//   credentials: true,
+//   origin: "http://localhost:5173"
+// }));
+
 app.use(cors({
   credentials: true,
-  origin: "http://localhost:5173"
+  origin: "https://totalfootball.vercel.app"
 }));
 
 
